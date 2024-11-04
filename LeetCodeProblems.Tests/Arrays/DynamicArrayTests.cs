d6aefa48321896ef249353b092831f6643ce9470
Windeko <tylerjameswoody@gmail.com>
2024-11-04 09:35:16 -0800

    added initial test for Get

--- LeetCodeProblems.Tests/Arrays/DynamicArrayTests.cs
+++ LeetCodeProblems.Tests/Arrays/DynamicArrayTests.cs
@@ -2,4 +2,22 @@
 
 public class DynamicArrayTests
 {
+    [Fact]
+    public void Get_GivenIndex_ReturnsElement()
+    {
+        // Arrange
+        DynamicArray arr = new(3);
+
+        arr.PushBack(3);
+        arr.PushBack(5);
+        arr.PushBack(1);
+
+        int expectedVal = 3;
+
+        // Act
+        int actual = arr.Get(0);
+
+        // Assert
+        Assert.Equal(expectedVal, actual);
+    }
 }
